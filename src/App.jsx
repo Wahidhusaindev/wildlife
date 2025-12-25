ddd360672b4181a784e8bb26630705b2ce458e03
yashika-bansal <byashika902@gmail.com>
2025-12-25 22:15:46 +0530

    hom

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,15 @@ import Program from "./pages/Program";
 import JoinTeam from "./pages/JoinTeam";
 import Contact from "./pages/Contact";
 import Blog from "./pages/Blog";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Home />
      <Routes>
-      <Route path="/" element={<Wildlife />} />
+      <Route path="/wildlife" element={<Wildlife />} />
       <Route path="/program" element={<Program />} />
       <Route path="/join-us" element={<JoinTeam />} />
       <Route path="/contact" element={<Contact />} />
